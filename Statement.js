a40cce65d200f8ca076edee61e206d20fe4f3f47
ShaunFlood <shaunflood@hotmail.co.uk>
2023-06-06 00:43:45 +0100

    Formatted the table and changed the mock testing to match

--- Statement.js
+++ Statement.js
@@ -1,17 +1,18 @@
-class Statement {
-    constructor(account) {
-        this.account = account;
-    }
+    class Statement {
+        constructor(account) {
+            this.account = account;
+        }
 
-    printStatement() {
-        const header = 'date || credit || debit || balance';
-        console.log(header);
-        const transactions = this.account.transaction;
-        transactions.forEach(({ credit, debit, date, balance }) => {
-            const transactionString = `${date} || ${credit ? credit.toFixed(2) : ''} || ${debit ? debit.toFixed(2) : ''} || ${balance.toFixed(2)}`;
-            console.log(transactionString);
-        });
+        printStatement() {
+            const columnWidth = 12;
+            const header = 'date || credit || debit || balance';
+            const formattedHeader = header.split(' || ').map(segment => segment.padEnd(columnWidth)).join(' || ');
+            console.log(formattedHeader);
+            const transactions = this.account.transaction;
+            transactions.forEach(({ credit, debit, date, balance }) => {
+                const transactionString = `${date.padEnd(columnWidth)} || ${credit ? credit.toFixed(2).padEnd(columnWidth) : ''.padEnd(columnWidth)} || ${debit ? debit.toFixed(2).padEnd(columnWidth) : ''.padEnd(columnWidth)} || ${balance.toFixed(2).padEnd(columnWidth)}`;
+                console.log(transactionString);
+            });
+        }
     }
-    
-}
-module.exports = Statement;
+    module.exports = Statement;
--- Statement.test.js
+++ Statement.test.js
@@ -6,34 +6,36 @@ describe('Statement functionality', () => {
         const account = new BankAccount()
         const statement = new Statement(account)
         expect(statement.account).toBe(account)
-    })
-
+    });
     it('When we try to print the statement it should show the header', () => {
         const account = new BankAccount()
         const statement = new Statement(account)
         const logSpy = jest.spyOn(console, 'log').mockImplementation();
         statement.printStatement();
-        expect(logSpy).toHaveBeenCalledWith('date || credit || debit || balance');
+        expect(logSpy).toHaveBeenCalledWith("date         || credit       || debit        || balance     ");
+        expect(logSpy).toHaveBeenCalledTimes(1)
         logSpy.mockRestore();
     });
     it('When we try to print out a transaction that is a deposit, it should not show any credit', () => {
         const account = new BankAccount();
         const statement = new Statement(account)
-        const logSpy = jest.spyOn(console, 'log').mockImplementation(); 
+        const logSpy = jest.spyOn(global.console, 'log').mockImplementation(); 
         account.deposit(1000)
-        statement.printStatement();
-        expect(logSpy).toHaveBeenCalledWith('date || credit || debit || balance');
-        expect(logSpy).toHaveBeenCalledWith(`${new Date().toLocaleDateString()} || 1000.00 ||  || 1000.00`);
+        statement.printStatement();  
+        expect(logSpy).toHaveBeenCalledWith((`${new Date().toLocaleDateString()}     || 1000.00      ||              || 1000.00     `))
+        expect(logSpy).toHaveBeenCalledTimes(2)
         logSpy.mockRestore();
     });
     it('When we try to print out a transaction that is a withdrawal, it should not show any debit', () => {
-    const account = new BankAccount();
+        const account = new BankAccount();
         const statement = new Statement(account)
         const logSpy = jest.spyOn(console, 'log').mockImplementation();
         account.balance = 5000;
         account.withdraw(2000)
         statement.printStatement();
-        expect(logSpy).toHaveBeenCalledWith('date || credit || debit || balance');
-        expect(logSpy).toHaveBeenCalledWith(`${new Date().toLocaleDateString()} ||  || 2000.00 || 3000.00`);
-    })
+        expect(logSpy).toHaveBeenCalledWith("date         || credit       || debit        || balance     ");
+        expect(logSpy).toHaveBeenCalledWith((`${new Date().toLocaleDateString()}     ||              || 2000.00      || 3000.00     `))
+        expect(logSpy).toHaveBeenCalledTimes(2)
+        logSpy.mockRestore();
+    });
 });
