7a5751a15eb8c570fb306940fa08245d1c4720c2
ShaunFlood <shaunflood@hotmail.co.uk>
2023-06-05 18:30:24 +0100

    Test passed for balance being 0

--- BankAccount.test.js
+++ BankAccount.test.js
@@ -1,4 +1,5 @@
-const BankAccount = require ('./Bank_Account.js')
+const BankAccount = require ('./BankAccount.js')
+
     describe('Bank account functionality', () => {
         it('When we check the balance it should return 0', () => {
         account = new BankAccount();
